7d6507ffb6e27b147f093588323fde92abcd57e1
Max Silva <maxjuandasilva@gmail.com>
2023-02-08 20:25:33 -0300

    Adicionado a Framework Bootstrap na seção de Habilidades.

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -6,11 +6,13 @@ import {ReactComponent as SharePoint} from './icons/microsoftsharepoint.svg';
 import {ReactComponent as Mysql} from './icons/mysql.svg';
 import {ReactComponent as Nodejs} from './icons/nodedotjs.svg';
 import {ReactComponent as Php} from './icons/php.svg';
+import {ReactComponent as Bootstrap} from './icons/bootstrap.svg';
 import {ReactComponent as Postgres} from './icons/postgresql.svg';
 import {ReactComponent as Reactjs} from './icons/react.svg';
 import {ReactComponent as StyledComponents} from './icons/styledcomponents.svg';
 import {ReactComponent as Javascript} from './icons/javascript.svg';
 import {ReactComponent as Typescript} from './icons/typescript.svg';
+
 
 export default function Skills() {
     
@@ -27,32 +29,36 @@ export default function Skills() {
                     <Css width={60}/>
                 </div>
                 <div>
-                    <StyledComponents width={60}/>
-                </div>
-                <div>
                     <Javascript width={60}/>
                 </div>
                 <div>
                     <Typescript width={60}/>
                 </div>
                 <div>
-                    <Php width={60}/>
-                </div>
-                <div>
-                    <Nodejs width={60}/>
+                    <StyledComponents width={60}/>
                 </div>
                 <div>
                     <Reactjs width={60}/>
                 </div>
                 <div>
+                    <Bootstrap width={60}/>
+                </div>
+                <div>
+                    <Nodejs width={60}/>
+                </div>
+                <div>
                     <SharePoint width={60}/>
                 </div>
+                <div>
+                    <Php width={60}/>
+                </div>
+                             
                 <div>
                     <Mysql width={60}/>
                 </div>
                 <div>
                     <Postgres width={60}/>
-                </div>                            
+                </div>                           
         </Information>
     </Container>
     )
